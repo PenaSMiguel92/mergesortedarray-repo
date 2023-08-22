2e96c9d84d83c88d864518572d025f89e26bf858
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-21 21:59:36 -0700

    faster solution
    my initial thought process, but got tempted into using Arrays class

--- Solution.java
+++ Solution.java
@@ -1,10 +1,32 @@
-import java.util.*;
-
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int index = m; index < m + n; index++) {
-            nums1[index] = nums2[index - m];
+        if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+            return;
         }
-        Arrays.sort(nums1);
+        int[] tmp1 = new int[m];
+        for (int i = 0; i < m; i++) {
+            tmp1[i] = nums1[i];
+        }
+
+        int left = 0;
+        int right = 0;
+        int cur = 0;
+        while (left < m && right < n) {
+            if (tmp1[left] <= nums2[right]) {
+                nums1[cur++] = tmp1[left++];
+            } else {
+                nums1[cur++] = nums2[right++];
+            }
+        }
+        while (left < m) {
+            nums1[cur++] = tmp1[left++];
+        }
+        while (right < n) {
+            nums1[cur++] = nums2[right++];
+        }
+
     }
 }
