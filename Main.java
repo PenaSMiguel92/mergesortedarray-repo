676ca7f4f9ddf886ac13753e16dc0b5f7abaff8c
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-21 21:11:21 -0700

    main method added

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
-public class Main { public static void main(String[] args) {}}
+import java.util.*;
+public class Main { 
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] array1 = new int[] { 1, 2, 3, 0, 0, 0 };
+        int[] array2 = new int[] { 1 };
+        int[] array3 = new int[] { 0 };
+        int[] array4 = new int[] { 3, 4, 5, 5, 0, 0 };
+
+        sol.merge(array1, 3, new int[] { 2, 5, 6 }, 3);
+        sol.merge(array2, 1, new int[] {}, 0);
+        sol.merge(array3, 0, new int[] { 1 }, 1);
+        sol.merge(array4, 4, new int[] { 2, 4 }, 2);
+        
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(array3));
+        System.out.println(Arrays.toString(array4));
+    }
+}
